6bee5e6771c097d9172f202679b8d045e2e569aa
Jérémy Michel <jeremymichel91@gmail.com>
2017-08-07 10:57:41 +0200

    Fix Turbolinks initialization, forcing the script to be placed in the <head> of every page. Fix also the annoying issue that may occur with the MediaElement plugin. Sometimes the player does not load and breaks the page. This is fixed by forcing the script responsible for the media embeds to be loaded on every page. A better solution is in the works, it works but it isn't optimal

--- README.md
+++ README.md
@@ -1,18 +1,7 @@
-#WP Turbolinks 5
+# MJ WP Turbolinks 5
+
+Fork with fixes of the original [WP Turbolinks 5 plugin](https://github.com/christophwolff/wp-turbolinks-5)
+
 A small Plugin to activate Turbolinks in your WordPress Frontend.
 
-You can find a [Demo](https://medienproduzent.net) on my Blog. Just navigate around.
-
 You can find more about Turbolinks [here](https://github.com/turbolinks/turbolinks).
-
-
-Features
---
-⚡️ Lightning Fast Frontend
-
-
-Known Issues
---
-- 🐞The WP Adminbar ist not compatible yet. Turbolinks only gets activated when you are **not** logged in. So to speak, if you are a normal visitor.
-
-
--- wp-turbolinks5.php
+++ wp-turbolinks5.php
@@ -1,28 +1,39 @@
 <?php
 /*
-Plugin Name: WP Turbolinks 5
-Plugin URI: http://christophwolff.de
+Plugin Name: MJ WP Turbolinks 5
+Plugin URI: http://misterjekyll.be
 Description: Make your WordPress Website fast with Turbolinks 5!
-Version: 0.3
-Author: Christoph Wolff
+Version: 0.4
+Author: Mister Jekyll
 */
 
-define('WPTURBOLINKS5_VERSION'    , '0.3');
-define('WPTURBOLINKS5_PLUGIN_NAME', 'WP Turbolinks 5');
+define('WPTURBOLINKS5_VERSION'    , '0.4');
+define('WPTURBOLINKS5_PLUGIN_NAME', 'MJ WP Turbolinks 5');
 define('WPTURBOLINKS5_FILE'       , __FILE__);
 define('WPTURBOLINKS5_PATH'       , realpath(plugin_dir_path(WPTURBOLINKS5_FILE)) . '/');
 define('WPTURBOLINKS5_URL'        , plugin_dir_url(WPTURBOLINKS5_FILE));
-define('WPTURBOLINKS5_LIB_URL', WPTURBOLINKS5_URL . '/js/turbolinks.min.js'); // (Turbolinks 5 BETA5)
+define('WPTURBOLINKS5_LIB_URL', WPTURBOLINKS5_URL . '/js/turbolinks.min.js');
+define('WPTURBOLINKS5_MEDIAELEMENT_URL', WPTURBOLINKS5_URL . '/js/mj_wp_medialement.min.js');
 
 /*
- * Init the Turbolinks Script. Nothing more. It just works.
+ * Init the Turbolinks script and fix the mediaelement not loading issues that may occur
  */
-function turolinks_init() {
+function mj_turbolinks_init() {
 	// TURBOLINKS 5 doenst like the Adminbar so we will have a peek if it exists
 	if(!is_admin() && !is_admin_bar_showing()) {
+
+		// Register and enqueue Turbolinks
 		wp_register_script('turbolinks', WPTURBOLINKS5_LIB_URL);
-		wp_enqueue_script( 'turbolinks', WPTURBOLINKS5_LIB_URL , array(), WPTURBOLINKS5_VERSION, true );	
+		wp_enqueue_script('turbolinks', WPTURBOLINKS5_LIB_URL , [], WPTURBOLINKS5_VERSION, false);
+
+		// Override wp-mediaelement.js file to work with turbolinks (avoid the annoying error that sometimes happens)
+		wp_deregister_script('wp-mediaelement');
+		wp_register_script('wp-mediaelement', WPTURBOLINKS5_MEDIAELEMENT_URL, ['mediaelement', 'jquery'], WPTURBOLINKS5_VERSION, true);
+
+		// Add wp-mediaelement and its dependencies to every page
+		wp_enqueue_style('wp-mediaelement');
+		wp_enqueue_script('wp-mediaelement');
 	}
 }
 
-add_action('init', 'turolinks_init');
+add_action('init', 'mj_turbolinks_init');
